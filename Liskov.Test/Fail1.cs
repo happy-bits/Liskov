4f8270a4aa78385901cd6e811e798a91d157ae09
Oscar Olsson <happybits@gmail.com>
2022-08-12 16:13:30 +0200

    -

---
+++ Liskov.Test/Fail1.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Liskov.Test
+{
+    [TestClass]
+    public class Fail1
+    {
+        class Rectangle
+        {
+            private double _width;
+            private double _height;
+
+            public double Area => _width * _height;
+
+            public double Width => _width;  
+            public double Height => _height;
+
+            public Rectangle(double width, double height)
+            {
+                _width = width;
+                _height = height;
+            }
+
+            public virtual void SetWidth(double width)
+            {
+                _width = width;
+            }
+
+            public virtual void SetHeight(double height)
+            {
+                _height = height;
+            }
+        }
+
+        class Square : Rectangle
+        {
+            public Square(double size) : base(size, size)
+            {
+            }
+            public override void SetHeight(double height)
+            {
+                base.SetHeight(height);
+                base.SetWidth(height);
+            }
+            public override void SetWidth(double width)
+            {
+                base.SetHeight(width);
+                base.SetWidth(width);
+            }
+        }
+
+        Rectangle ThreeByFour(Rectangle rectangle)
+        {
+            rectangle.SetWidth(3);
+            rectangle.SetHeight(4);
+            return rectangle;
+        }
+
+        [TestMethod]
+        public void HappyPath()
+        {
+            var rectangle = new Rectangle(2, 2);
+            var adjusted = ThreeByFour(rectangle);
+            adjusted.Area.ShouldBe(12);
+        }
+
+        [TestMethod]
+        public void HappyPath2()
+        {
+            var square = new Square(2);
+            square.Width.ShouldBe(2);
+            square.Height.ShouldBe(2);
+
+            square.SetWidth(5);
+            square.Width.ShouldBe(5);
+            square.Height.ShouldBe(5);
+        }
+
+
+        [TestMethod]
+        public void WillFail()
+        {
+            var square = new Square(2);
+            var adjusted = ThreeByFour(square);
+            adjusted.Area.ShouldBe(12);  // Fail: will be 16
+        }
+    }
+}
